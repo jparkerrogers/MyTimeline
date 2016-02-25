54bba7bb3d20586f9f6fc2bd4085c0332961d3c2
Dylan Slade <dylan.slade@icloud.com>
2016-02-25 00:22:03 -0700

    Part 2 class implementation complete

--- Timeline/LoginSignUpViewController.swift
+++ Timeline/LoginSignUpViewController.swift
@@ -10,5 +10,83 @@ import Foundation
 import UIKit
 
 class LoginSignUpViewController: UIViewController {
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var websiteURLTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    var mode: ViewMode = .Signup
+    var fieldsAreValid: Bool {
+        get {
+            if mode == ViewMode.Login {
+                if userNameTextField.text!.isEmpty || emailTextField.text != "" || passwordTextField.text != "" {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                if userNameTextField.text != "" || emailTextField.text != "" || passwordTextField.text != "" || bioTextField.text != "" || websiteURLTextField.text != "" {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+    
+    enum ViewMode {
+        case Login
+        case Signup
+    }
+    
+    @IBAction func actionButtonTapped(sender: UIButton) {
+        if fieldsAreValid {
+            switch mode {
+            case .Signup:
+                UserController.createUser(emailTextField.text!, userName: userNameTextField.text!, password: passwordTextField.text!, bio: bioTextField.text, url: websiteURLTextField.text, completion: { (success, user) -> Void in
+                    if success {
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        self.presentAlertAfterFailedAuthentication("Failed to signup, Check fields adn try again", message: "Yo go gurl")
+                    }
+                })
+                return
+            case .Login:
+                UserController.authenticateUser(emailTextField.text!, password: passwordTextField.text!, completion: { (success) -> Void in
+                    if success {
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        self.presentAlertAfterFailedAuthentication("Yooogz", message: "yola")
+                    }
+                })
+                return
+            }
+        } else {
+            
+        }
+    }
+    
+    func presentAlertAfterFailedAuthentication(alertTitle: String, message: String) {
+        let alertController = UIAlertController(title: alertTitle, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let alertAction1 = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        alertController.addAction(alertAction1)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func updateViewBasedOnMode() {
+        switch mode {
+        case .Login:
+            bioTextField.hidden = true
+            websiteURLTextField.hidden = true
+            actionButton.setTitle("Login", forState: UIControlState.Normal)
+        case .Signup:
+            actionButton.setTitle("Signup", forState: UIControlState.Normal)
+        }
+    }
+    
+    override func viewDidLoad() {
+        updateViewBasedOnMode()
+    }
     
 }
